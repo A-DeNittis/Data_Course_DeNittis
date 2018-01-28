475d652440183ae145ec96d3d7ddf4b6887ce6f9
Aly DeNittis <Alyson.Ova@gmail.com>
2018-01-28 14:16:16 -0700

    Week 3

--- Assignment_3_Worked.R
+++ Assignment_3_Worked.R
@@ -21,12 +21,12 @@ vector4 = c("stuff","junk","things")
 c(vector3, vector4)
 
 ?rep #rep replicates the values in x. It is a generic function, and the (internal) default method is described here.
-rep("vector3",3)
+rep(vector3,3)
 
 #############################################################################
 
 
-setwd("C:/Users/Aly/Desktop/Data_Course/data/")
+setwd("C:/Users/Aly/Desktop/GIT/Data_Course/data/")
 list.files()
 
 dat = read.csv("thatch_ant.csv")
@@ -77,7 +77,7 @@ plot(nums, main = "My Title", xlab = "My axis label", ylab = "My other axis labe
 
 
 ?jpeg() #run next 3 lines together
-jpeg(filename = "C:/Users/Aly/Desktop/Data_Course_Denittis/TestPlot.jpeg")
+jpeg(filename = "C:/Users/Aly/Desktop/GIT/Data_Course_Denittis/TestPlot.jpeg")
 plot(nums, main = "My Title", xlab = "My axis label", ylab = "My other axis label", col = c(1,2))
 dev.off()
 ?dev.off
@@ -139,8 +139,11 @@ df1 # look at it...note column names are what we gave it.
 # save it into an object called "dat3"
 
 row.names(dat2)
-
-
+?head # n is number of elements for a vecter, n=20 will return first 20 lines, n=5 5 lines, etc.
+colColony = head(c(dat2$Headwidth), n=20)
+colMass = head(c(dat2$Mass), n=20)
+ColMassDF = data.frame(Colony = colColony, Mass = colMass)
+write.csv(ColMassDF, file = "C:/Users/Aly/Desktop/GIT/Data_Course_DeNittis/ColMassDF_Asgnt3Practice.csv", row.names = FALSE)
 
 ###### WRITING OUT FILES FROM R #######
 ?write.csv()
@@ -170,6 +173,9 @@ for(i in levels(dat2$Size.class)){
 }
 
 
+levels(dat2$Size.class)
+class(dat2$Size.class)
+
 # more complex:
 # define a new vector or data frame outside the for loop first
 new_vector = c() # it's empty
@@ -192,24 +198,54 @@ new_vector
 
 #fill it in
 size_class_mean_mass = data.frame(Size_Class = levels(dat$Size.class), Mean = new_vector)
-view(size_class_mean_mass)
-
-
-
+size_class_mean_mass
 
 ############ YOUR HOMEWORK ASSIGNMENT ##############
+##Objects
+setwd("C:/Users/Aly/Desktop/GIT/Data_Course/data/")
+dat = read.csv("thatch_ant.csv")
+a = which(dat$Headwidth == "41mm")
+dat$Headwidth[a] = "41.000"
+dat$Headwidth = as.numeric(dat$Headwidth)
+which(dat$Headwidth == NA)
+bad = which(dat$Headwidth == 1)
+dat$Headwidth[bad] = NA
+dat2 = na.omit(dat)
 
 # 1.  Make a scatterplot of headwidth vs mass. See if you can get the points to be colored by "Colony"
+install.packages("RColorBrewer")
+library(RColorBrewer)
+colfact = as.factor(dat2$Colony)
+nlevels(colfact) #I just wanted to see how many levels there were (ie colonies) for coloration (11 levels = 11 colonies = 11 colors)
+display.brewer.all()
+cols <- brewer.pal(n=11, name = "Spectral")
+colsColny <- cols[dat2$Colony]
+plot(dat2$Headwidth, dat2$Mass, main = "Thatch Ant Headwidth vs Mass", xlab = "Headwidth (mm)", ylab = "Mass (unit)", col = colsColny, pch = 20)
+legend("topleft",legend = paste(rep(c("Colony"), times = 11), c(1:11)), col = cols, pch = 20)
 
+## I know there is extra stuff that wasn't required for the task, but I found some 
+## helpful things like the legend command and color packages, and figured it might be a good place to learn and apply. 
 
 # 2.  Write the code to save it (with meaningful labels) as a jpeg file
 
+jpeg(filename = "C:/Users/Aly/Desktop/GIT/Data_Course_DeNittis/Thatch_Ant_Plot.jpeg", width = 700, height = 550, quality = 100)
+plot(dat2$Headwidth, dat2$Mass, main = "Thatch Ant Headwidth vs Mass", xlab = "Headwidth (mm)", ylab = "Mass (unit)", col = colsColny, pch = 20)
+legend("topleft",legend = paste(rep(c("Colony"), times = 11), c(1:11)), col = cols, pch = 20)
+dev.off()
 
 # 3.  Subset the thatch ant data set to only include ants from colony 1 and colony 2
 
+dat2subset = dat2[dat2$Colony <= 2,] #Shorter/convenient for specific task, but limited
+##OR
+dat2subset2 = subset(dat2, Colony == 1 | Colony == 2) #More appropriate for broader application
+
+## I wasn't sure if you wanted us to use the for loop for this task, seeing as it is what we covered, but when
+## I was trying to set it up (maybe incorrectly) the command I was using could stand alone (see above) and the extra
+## script for the for loop seemed superfluous. 
+
 
 # 4.  Write code to save this new subset as a .csv file
-
+write.csv(dat2subset, "C:/Users/Aly/Desktop/GIT/Data_Course_DeNittis/Thatch_Ant_Subset.csv")
 
 # 5.  Upload this R script (with all answers filled in and tasks completed) to canvas
       # I should be able to run your R script and get all the plots created and saved, etc.
